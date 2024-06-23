eecbdbf6217257320dce9ea5f9d74de0b34a0390
Rkern91 <156999@upf.br>
2024-06-23 20:25:42 -0300

    Melhorias gerais - Ajuste para tornar a porta de configuracao do banco dinamica

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   migrationsTableName: 'migrations',
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: 5432,
+  port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
